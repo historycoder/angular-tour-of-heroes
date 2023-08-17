a26fb63b7bcdd4be7c6581babc756da14ab08aa7
Lauren Mitchell <sortsearch@gmail.com>
2023-08-17 12:17:23 -0400

    Test Fix: Stub heroes component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,38 @@
-import { TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
+
+@Component({selector: 'app-heroes', template:''})
+class HeroesStubComponent {}
+
+let comp: AppComponent;
+let fixture: ComponentFixture<AppComponent>;
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    declarations: [
+      AppComponent,
+      HeroesStubComponent
+    ]
+  }).compileComponents().then(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    comp = fixture.componentInstance;
   }));
 
+  tests();  
+});
+
+function tests() {
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(comp).toBeTruthy();
   });
 
   it('should render header', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Tour of Heroes');
   });
-});
+}
